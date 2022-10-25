614eeff6788558f0c7ebbe15194d8806cf27e0ac
NK.Shien <lithoshu@yile.com.tw>
2022-10-25 13:01:33 +0800

    1.新增基礎資料夾、腳本

---
+++ Assets/Scripts/GameCore/Tests/Editor/RhythmCollectGameModelTest.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using GameCore;
+
+public class RhythmCollectGameModelTest
+{
+
+}
