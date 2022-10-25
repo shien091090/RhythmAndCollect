08a4e0199ad64d5ea67fc43401c6ed4a4572cc7b
NK.Shien <lithoshu@yile.com.tw>
2022-10-25 18:35:07 +0800

    1.新增空的Test Case

--- Assets/Scripts/GameCore/Tests/Editor/RhythmCollectGameModelTest.cs
+++ Assets/Scripts/GameCore/Tests/Editor/RhythmCollectGameModelTest.cs
@@ -7,5 +7,33 @@ using GameCore;
 
 public class RhythmCollectGameModelTest
 {
+    [Test]
+    public void LogicTest_ClickCollectItem()
+    {
 
+    }
+
+    [Test]
+    public void LogicTest_CreateHeading()
+    {
+
+    }
+
+    [Test]
+    public void LogicTest_SpawnCollectItem()
+    {
+
+    }
+
+    [Test]
+    public void LogictTest_DestroyCollectItem()
+    {
+
+    }
+
+    [Test]
+    public void LogicTest_SetHp()
+    {
+
+    }
 }
