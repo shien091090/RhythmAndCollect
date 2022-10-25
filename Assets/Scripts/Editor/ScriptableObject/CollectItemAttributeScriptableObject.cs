994dc90b7c2618e6cc82a23c542d98e4a78d7b48
NK.Shien <lithoshu@yile.com.tw>
2022-10-25 18:36:07 +0800

    1.新增收集物件的屬性定義SO

---
+++ Assets/Scripts/Editor/ScriptableObject/CollectItemAttributeScriptableObject.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace GameCore
+{
+    public class CollectItemAttributeScriptableObject : ScriptableObject
+    {
+        [SerializeField] private List<string> attributeTypeNames;
+
+        public List<string> GetAttributeTypeNames { get { return attributeTypeNames; } }
+    }
+
+}
