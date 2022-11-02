e1cba9f6e230bf8a778aa02328b190102081ff15
NKShien_Home <shien091091@gmail.com>
2022-11-02 21:30:10 +0800

    1.補上ColletctItem建構子參數

--- Assets/Scripts/GameCore/Model/RhythmCollectGame/DummyCollectItemSpawnAttribute.cs
+++ Assets/Scripts/GameCore/Model/RhythmCollectGame/DummyCollectItemSpawnAttribute.cs
@@ -7,9 +7,9 @@ namespace GameCore
         public RhythmCollectItemAttribute GetCollectItemAttribute(List<RhythmCollectItem> currentAliveItemList, string[] currentHeadings)
         {
             if (currentAliveItemList == null || currentAliveItemList.Count <= 5) //場上的物件在5個以下時, 產生符合題目的Attribute
-                return new RhythmCollectItemAttribute(string.Empty, currentHeadings, 10, 10);
+                return new RhythmCollectItemAttribute(string.Empty, currentHeadings, 10, 10, 10);
             else //場上的物件在5個以上時, 產生不符合題目的Attribute
-                return new RhythmCollectItemAttribute(string.Empty, null, 10, 10);
+                return new RhythmCollectItemAttribute(string.Empty, null, 10, 10, 10);
 
         }
     }
