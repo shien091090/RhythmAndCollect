da8d7c0bf0edca3118093fbc4ffa1b0e4fbe1cc9
NK.Shien <lithoshu@yile.com.tw>
2022-11-03 10:26:39 +0800

    1.測試生成題目衝突時重新再生成的狀況

--- Assets/Scripts/GameCore/Model/RhythmCollectGame/DummyHeadingCreator.cs
+++ Assets/Scripts/GameCore/Model/RhythmCollectGame/DummyHeadingCreator.cs
@@ -3,28 +3,35 @@
     public class DummyHeadingCreator : IRhythmCollectGameHeadingCreator
     {
         private bool isOnlyOneResultForDummy;
+        private int creatorIndex;
 
         public DummyHeadingCreator(bool _isOnlyOneResult = false)
         {
             isOnlyOneResultForDummy = _isOnlyOneResult;
+            creatorIndex = 0;
         }
 
         public string[] CreateHeadings()
         {
             if (isOnlyOneResultForDummy)
                 return new string[] { "紅色", "香菇", "翅膀" };
-
-            System.Random dice = new System.Random();
-            int index = dice.Next(0, 3);
-
-            if (index == 0)
-                return new string[] { "紅色", "長的" };
-            else if (index == 1)
-                return new string[] { "紅色", "木頭", "翅膀" };
-            else if (index == 2)
-                return new string[] { "白色", "香菇" };
             else
-                return new string[0];
+            {
+                string[] resultHeadings = null;
+
+                if (creatorIndex == 0)
+                    resultHeadings = new string[] { "紅色", "長的" };
+                else if (creatorIndex == 1)
+                    resultHeadings = new string[] { "紅色", "木頭", "翅膀" };
+                else
+                    resultHeadings = new string[] { "白色", "香菇" };
+
+                creatorIndex++;
+                if (creatorIndex > 3)
+                    creatorIndex = 0;
+
+                return resultHeadings;
+            }
         }
 
         public int GetPossibleCreateHeadingsCount()
